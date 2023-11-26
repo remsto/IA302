01397bbe948a711e660439a2b5b074037228e2d7
remsto <remi.gastaud@hotmail.com>
2023-11-26 12:38:10 +0100

    added interval initialization

--- main.cpp
+++ main.cpp
@@ -36,8 +36,8 @@ vector<Constraint> readConstraintFromFile(string filename){
   return constraints;
 }
 
-bool checkTriangleInequality(vector<Constraint> &constraints){
-  // Find the number of points
+unsigned int nbPoints(vector<Constraint> &constraints){ 
+  // Gives the number of points
   unsigned int n = 0;
   for (unsigned int i=0; i<constraints.size(); i++){
     Constraint c = constraints[i];
@@ -48,6 +48,12 @@ bool checkTriangleInequality(vector<Constraint> &constraints){
       n = c.id2;
     }
   }
+  return n;
+}
+
+bool checkTriangleInequality(vector<Constraint> &constraints){
+  // Find the number of points
+  unsigned int n = nbPoints(constraints);
   vector<Constraint> neighbors[n]; // neighbors[i] is the list of constraints that involve point i
   for (unsigned int i=0; i<constraints.size(); i++){
     Constraint c = constraints[i];
@@ -186,9 +192,99 @@ void branchAndContract(IntervalVector start_interval, vector<Constraint> constra
   file.close();
 }
 
+IntervalVector InitializeIntervals(vector<Constraint> constraints, double tau){
+  unsigned int n = nbPoints(constraints);
+  IntervalVector start_interval(3*n);
+  start_interval[0] = Interval(0).inflate(tau/2);
+  start_interval[1] = Interval(0).inflate(tau/2);
+  start_interval[2] = Interval(0).inflate(tau/2);
+  vector<Constraint> neighbors[n]; // neighbors[i] is the list of constraints that involve point i
+  for (unsigned int i=0; i<constraints.size(); i++){
+    Constraint c = constraints[i];
+    neighbors[c.id1-1].push_back(c);
+    neighbors[c.id2-1].push_back(c);
+  }
+  // Find the first triangle
+  Constraint c1, c2, c3;
+  bool triangle_exists = false;
+  for (unsigned int i=0; i<n; i++){
+    for (Constraint neighbor: neighbors[i]){
+      unsigned int id_neighbor = (neighbor.id1-1 == i) ? neighbor.id2-1 : neighbor.id1-1;
+      for (Constraint neighbor2: neighbors[id_neighbor]){
+        unsigned int id_neighbor2 = (neighbor2.id1-1 == id_neighbor) ? neighbor2.id2-1 : neighbor2.id1-1;
+        if (id_neighbor2 == i) // Avoid the same point
+          continue;
+        for (Constraint neighbor3: neighbors[id_neighbor2]){
+          unsigned int id_neighbor3 = (neighbor3.id1-1 == id_neighbor2) ? neighbor3.id2-1 : neighbor3.id1-1;
+          if (id_neighbor3 == i){
+            c1 = neighbor;
+            c2 = neighbor2;
+            c3 = neighbor3;
+            triangle_exists = true;
+            break;
+          }
+        }
+        if (triangle_exists)
+          break;
+      }
+      if (triangle_exists)
+        break;
+    }
+    if (triangle_exists)
+      break;
+  }
+  if (!triangle_exists){
+    cout << "Error: no triangle found" << endl;
+    exit(1);
+  }
+  // Find the points and their distances in the first triangle
+  unsigned int id1 = c1.id1-1;
+  unsigned int id2 = c1.id2-1;
+  Interval d12 = c1.distance;
+  unsigned int id3 = (c2.id1-1 == id1 || c2.id1-1 == id2) ? c2.id2-1 : c2.id1-1;
+  Interval d13 = (c2.id1-1 == id1 || c2.id2-1 == id1) ? c2.distance : c3.distance;
+  Interval d23 = (c2.id1-1 == id2 || c2.id2-1 == id2) ? c2.distance : c3.distance;
+
+  // Initialize the first three points
+  start_interval[id1*3] = Interval(0).inflate(tau/2);
+  start_interval[id1*3+1] = Interval(0).inflate(tau/2);
+  start_interval[id1*3+2] = Interval(0).inflate(tau/2);
+  start_interval[id2*3] = c1.distance;
+  start_interval[id2*3+1] = Interval(0).inflate(tau/2);
+  start_interval[id2*3+2] = Interval(0).inflate(tau/2);
+
+  // Intersection of two circles
+  double coeff1 = 1/2;
+  Interval coeff2 = (d13*d13 - d23*d23)/(2*d12*d12);
+  Interval coeff3 = (1/2)*sqrt(2*(d13*d13+d23*d23)/(d12*d12) - (d13*d13-d23*d23)*(d13*d13-d23*d23)/(d12*d12*d12*d12) - 1);
+  start_interval[id3*3] = coeff1*(start_interval[id1*3] + start_interval[id2*3]) + coeff2*(start_interval[id2*3] - start_interval[id1*3]) + coeff3*(start_interval[id2*3+1] - start_interval[id1*3+1]);
+  start_interval[id3*3+1] = coeff1*(start_interval[id1*3+1] + start_interval[id2*3+1]) + coeff2*(start_interval[id2*3+1] - start_interval[id1*3+1]) + coeff3*(start_interval[id1*3] - start_interval[id2*3]);
+  start_interval[id3*3+2] = Interval(0).inflate(tau/2);
+
+  // Sum all the distances in the constraint
+  double max_dist = 0;
+  for (unsigned int i=0; i<constraints.size(); i++){
+    Constraint c = constraints[i];
+    max_dist += c.distance.diam();
+  }
+  // Initialize the rest of the points
+  for (unsigned int i=0; i<n; i++){
+    if (i == id1 || i == id2 || i == id3)
+      continue;
+    start_interval[i*3] = Interval(0).inflate(max_dist/2);
+    start_interval[i*3+1] = Interval(0).inflate(max_dist/2);
+    start_interval[i*3+2] = Interval(0).inflate(max_dist/2);
+  }
+  return start_interval;
+}
+
 int main(int argc, char** argv) {
   vector<Constraint> constraints = readConstraintFromFile("dgsol-1.3/data/data_set_1/graph.01.data");
   cout << "Read " << constraints.size() << " constraints" << endl;
+  cout << "Initializing intervals..." << endl;
+  double TAU = 0.1;
+  IntervalVector start_interval = InitializeIntervals(constraints, TAU);
+  cout << "Intervals initialized" << endl;
   cout << "Checking triangle inequality..." << endl;
   if (!checkTriangleInequality(constraints)){
     cout << "Triangle inequality is violated" << endl;
@@ -196,6 +292,9 @@ int main(int argc, char** argv) {
   }
   cout << "Triangle inequality is satisfied" << endl;
   cout << "New constraints size: " << constraints.size() << endl;
+  cout << "Starting branch and contract..." << endl;
+  branchAndContract(start_interval, constraints, TAU);
+  cout << "Branch and contract finished" << endl;
   // Print all the constraints
   // for (unsigned int i=0; i<constraints.size(); i++){
   //   Constraint c = constraints[i];
